fd23f967be088d3f43816f1aa8733e0e45a1a377
Mohammad Afraz <110837331+MohammadAfraz@users.noreply.github.com>
2024-02-14 23:24:18 +0530

    Delete RateLimiter-TokenBucket directory
    Pushing new project directory

--- RateLimiter-TokenBucket/src/com/demo/bucket/exceptions/TokenNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package com.demo.bucket.exceptions;
-
-public class TokenNotAvailableException extends RuntimeException {
-    public TokenNotAvailableException(String message){
-        super(message);
-    }
-}
